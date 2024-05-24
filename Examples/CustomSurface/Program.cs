f636399c6374bfc712c980ce5401e61c4624746c
Nick Klingensmith <programmerpichu@gmail.com>
2024-05-23 22:49:26 -0700

    Add extra sample project.

---
+++ Examples/CustomSurface/Program.cs
@@ -0,0 +1,12 @@
+﻿using StereoKit;
+using StereoKit.DesktopMirror;
+
+SK.Initialize(new SKSettings{assetsFolder="Assets"});
+
+DesktopMaterial desktop = new DesktopMaterial();
+desktop.Start();
+
+SK.Run(() => {
+    desktop.Step();
+    Mesh.Cube.Draw(desktop.Material, Matrix.TS(0,0,-0.5f, 0.2f));
+});
